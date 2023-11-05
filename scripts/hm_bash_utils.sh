8225b085834ce43765098a84d559b4b5d578939d
Alfred <alfred.addison.cq@gmail.com>
2023-11-05 01:56:27 -0500

    chore 🔨: Add 'Cloud9 Environment Configuration' and utility scripts

---
+++ scripts/hm_bash_utils.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# Utility Functions for Package Installation Script
+# ------------------------------------------------
+# This script provides a set of utility functions to be sourced by other shell
+# scripts for installing packages and reporting their status. It is intended to
+# standardize the output and error handling for package installations.
+#
+# Functions:
+#   print_title
+#   print_success
+#   print_failed
+#   print_finish
+#   install_package
+#
+# Usage:
+#   Source this script in other installation scripts to utilize its functions.
+#
+# -------------------------------------------------------------------------------------
+# Author:      Harmony
+# Created on:  05 nov 2023
+# Version:     1.0
+# License:     MIT License
+# -------------------------------------------------------------------------------------
+
+# Function: print_title
+# Description: Prints a title for the installation step in bold blue color.
+# Usage: print_title "Title of the Step"
+# Parameters:
+#   $1 - The title text to be printed
+print_title() {
+  echo -e "\e[1;34m=== $1 ===\e[0m"
+}
+
+# Function: print_success
+# Description: Prints a success message in bold green color indicating the
+#              successful installation of a package.
+# Usage: print_success "Package Name"
+# Parameters:
+#   $1 - The name of the package that was successfully installed
+print_success() {
+  echo -e "\e[1;32m$1 installed successfully.\e[0m"
+}
+
+# Function: print_failed
+# Description: Prints a failure message in bold red color indicating that the
+#              installation of a package has failed and refers to the log file.
+# Usage: print_failed "Package Name"
+# Parameters:
+#   $1 - The name of the package that failed to install
+print_failed() {
+  echo -e "\e[1;31mFailed to install $1. Check the log for details.\e[0m"
+}
+
+# Function: print_finish
+# Description: Prints a message in bold cyan color indicating that the installation
+#              process has finished.
+# Usage: print_finish
+print_finish() {
+  echo -e "\e[1;36mInstallation process finished.\e[0m"
+}
+
+# Function: install_package
+# Description: Attempts to install a package and prints a status message. Logs errors
+#              to a specified file.
+# Usage: install_package "Package Name" "install_command" "error_log_file"
+# Parameters:
+#   $1 - The name of the package to install
+#   $2 - The command that installs the package
+#   $3 - The file path for logging errors
+install_package() {
+  package_name=$1
+  install_command=$2
+  error_log_file=$3
+
+  print_title "Installing $package_name"
+  if eval $install_command > /dev/null 2>> $error_log_file; then
+    print_success $package_name
+  else
+    print_failed $package_name
+  fi
+}

7064b4d38f404f080abdb6b5c0c5a4ad8d48b4ca
Alfred <alfred.addison.cq@gmail.com>
2023-11-06 18:36:34 -0500

    feat(ui) 💄: Added Harmony main search page

--- Harmony/routes.py
+++ Harmony/routes.py
@@ -1,10 +1,13 @@
-from flask import render_template, jsonify, current_app, Blueprint, redirect, url_for
+from flask import render_template, jsonify, request, Blueprint, redirect, url_for
 import os
 import json
 
 bp = Blueprint("main", __name__)
 
 
-@bp.route("/")
+@bp.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        query = request.form["search"]
+        return redirect(url_for("search_results", query=query))
     return render_template("index.html")
